b0a5a6a37d76e5c815c92aa2eaba7f8d5ec16ac2
cristik <zhenghua.ni@zhangmen.com>
2020-09-29 04:57:53 +0800

    sharding jdbc demo

---
+++ samples-spring-components/sample-spring-entity/src/main/java/com/cristik/sample/entity/sharding/SampleSharding.java
@@ -0,0 +1,42 @@
+package com.cristik.sample.entity.sharding;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author zhenghua.ni
+ */
+@Data
+@TableName(value = "sample_sharding")
+public class SampleSharding {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField(value = "user_id")
+    private Long userId;
+
+    @TableField(value = "name")
+    private String name;
+
+    @TableField(value = "status")
+    private Integer status;
+
+    @TableField(value = "create_time")
+    private Date createTime;
+
+    @TableField(value = "create_by")
+    private Long createBy;
+
+    @TableField(value = "update_time")
+    private Date updateTime;
+
+    @TableField(value = "update_by")
+    private Long updateBy;
+
+}
