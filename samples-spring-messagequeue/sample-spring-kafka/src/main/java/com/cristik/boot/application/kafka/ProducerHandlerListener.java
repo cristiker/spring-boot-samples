5401b047752396263df455ca9edd9dd0ade9af60
cristik <cristik1991@gmail.com>
2021-02-10 02:42:26 +0800

    rename

--- samples-spring-messagequeue/sample-spring-kafka/src/main/java/com/cristik/boot/application/kafka/ProducerHandlerListener.java
+++ samples-spring-messagequeue/sample-spring-kafka/src/main/java/com/cristik/boot/application/kafka/ProducerHandlerListener.java
@@ -1,5 +1,6 @@
 package com.cristik.boot.application.kafka;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.support.ProducerListener;
 
@@ -8,18 +9,28 @@ import org.springframework.kafka.support.ProducerListener;
  */
 public class ProducerHandlerListener implements ProducerListener {
 
-    @Override
+//    @Override
     public void onSuccess(String topic, Integer partition, Object key, Object value, RecordMetadata recordMetadata) {
         System.out.println("成功");
     }
 
-    @Override
+//    @Override
     public void onError(String topic, Integer partition, Object key, Object value, Exception exception) {
         System.out.println("失败");
     }
 
-    @Override
+//    @Override
     public boolean isInterestedInSuccess() {
         return false;
+    }
+
+    @Override
+    public void onSuccess(ProducerRecord producerRecord, RecordMetadata recordMetadata) {
+
+    }
+
+    @Override
+    public void onError(ProducerRecord producerRecord, Exception exception) {
+
     }
 }
